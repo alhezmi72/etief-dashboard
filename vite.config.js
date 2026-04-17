710ad330e1e2a9a6e22a1e51087ccd4e3936d063
alhezmi72 <alhezmi@yahoo.de>
2026-04-17 09:24:05 +0300

    update build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
- // base: '/etief-dashboard/',
+ base: '/etief-dashboard/',
   plugins: [
     tailwindcss(),
     react({
