cbfb376f8471ec1d8010ace1b4abe70ebaf94421
alhezmi72 <alhezmi@yahoo.de>
2026-08-02 09:30:21 +0300

    update config

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const repoName = 'etief-dashboard'
-
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? `/${repoName}/` : '/',
-  build: {
-    outDir: 'docs',
-    emptyOutDir: true,
-  },
+export default defineConfig({
+  //base: './',
+  base: './',
   plugins: [
     tailwindcss(),
     react({
@@ -18,5 +13,5 @@ export default defineConfig(({ command }) => ({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
-  ],
-}))
+  ]
+})
